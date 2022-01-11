a87cf63be90d96bf8549aeca8a7ad5305361c44f
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-01-11 09:00:56 +0100

    Adding root docs link
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  title: 'Arcsecond Pro Tools Documentation',
+  title: 'Arcsecond Pro Tools',
   description: 'The documentation about all the tools of www.arcsecond.io, and iObserve on the web most notably.',
   base: '/tools/',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'iObserve', link: '/iobserve/' }
+      { text: 'iObserve', link: '/iobserve/' },
+      { text: 'Arcsecond Docs', link: 'https://docs.arcsecond.io' }
     ]
   }
 }
